8941d12bcf1a8e6ddb50ac3a247f340a540c1d5c
Andrew Johnson <andrewjohnson@Andrews-MBP.home>
2017-01-08 14:20:13 -0500

    update example to use blank agent

--- example/docker/src/main.rs
+++ example/docker/src/main.rs
@@ -1,6 +1,11 @@
 extern crate rust_openai;
 use rust_openai::docker;
 
+
+
 fn main() {
-   rust_openai::docker::spawn_env();
+   let a = rust_openai::docker::Agent{};
+   let mut d = rust_openai::docker::Gym::new();
+   d.parse_args();
+   d.start(a);
 }
