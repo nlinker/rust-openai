c4be98f63c163bed387caea3feadbd9f8a2c269b
Andrew Johnson <andrewjohnson@Andrews-MBP.home>
2017-01-10 16:11:25 -0500

    project can just be "openai""

--- example/docker/src/main.rs
+++ example/docker/src/main.rs
@@ -1,16 +1,17 @@
-extern crate rust_openai;
+extern crate openai;
+use openai::*;
 
 struct NullGymMember;
-impl rust_openai::GymMember for NullGymMember {
-   fn start (&mut self, s: rust_openai::GymShape) -> &mut rust_openai::GymMember { return self }
-   fn reward(&mut self, r: rust_openai::gym_reward, d: rust_openai::gym_done) {}
+impl GymMember for NullGymMember {
+   fn start (&mut self, s: GymShape) -> &mut GymMember { return self }
+   fn reward(&mut self, r: gym_reward, d: gym_done) {}
    fn reset(&mut self) {}
    fn close(&mut self) {}
 }
 
 fn main() {
    let mut m = NullGymMember{};
-   let mut d = rust_openai::Gym::new();
+   let mut d = Gym::new();
    d.parse_args();
    d.start(m);
 }
